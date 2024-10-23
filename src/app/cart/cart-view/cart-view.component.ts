08888246ef4968ce19e287c5a6f171d2f26781be
snigdha <sunny.rangineni@gmail.com>
2024-10-23 12:42:38 -0500

    calculating the total price

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -9,6 +9,7 @@ import { Product } from 'src/app/models/product';
 })
 export class CartViewComponent implements OnInit{
   cartItems: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService){
 
@@ -17,6 +18,16 @@ export class CartViewComponent implements OnInit{
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     })
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for(let item of this.cartItems)
+      {
+        total += item.price;
+      }
+    return total;
   }
 }
